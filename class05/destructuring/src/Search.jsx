8a4d0a27223b69a61564b5f15ca624bf775fa856
asamamun <asamamun@gmail.com>
2026-04-01 18:41:12 +0600

    removed unzipped dir

--- class05/destructuring/src/Search.jsx
+++
@@ -1,9 +0,0 @@
-const Search = ({search, handleSearch}) => {
-
-    return (
-        <div>
-            <input type="text" placeholder="Search..." value={search} onChange={(e) => handleSearch(e)} />
-        </div>
-    )
-}
-export default Search;
